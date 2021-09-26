b411bd9d321da9aad3596ef0be8b7947c8bab505
Giedrius <giedrius.svazinskas@3ds.com>
2021-09-26 21:48:49 +0300

    github pages

--- src/components/CreateEditForm/__tests__/CreateEditForm.test.tsx
+++ src/components/CreateEditForm/__tests__/CreateEditForm.test.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import CreateEditForm from './CreateEditForm';
+import CreateEditForm from '../CreateEditForm';
+import { FamilyMember } from '../../FamilyTree/types';
 
 describe('<CreateEditForm />', () => {
     test('it should mount', () => {
-        render(<CreateEditForm/>);
+        render(<CreateEditForm selectedMember={{} as FamilyMember}
+                               isEditMode={false}
+                               handleClose={() => ({})}/>);
 
         const createEditForm = screen.getByTestId('CreateEditForm');
 
